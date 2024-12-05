705fdd5847f72cbe72bcb8460db48841159e20e6
MathieuWil <mathieu0108w@gmail.com>
2024-12-05 14:39:00 +0100

    Creation_du_projet

---
+++ chatoid/src/main/java/com/example/chatoid/ChatoidApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatoid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatoidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatoidApplication.class, args);
+	}
+
+}
